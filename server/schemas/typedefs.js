07bbd330a0a8d251b354052acef94dec5401f3db
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-21 15:27:00 -0600

    Mutation in resolvers is coming together.

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
     type User {
@@ -20,7 +20,17 @@ const typeDefs = gql`
     }
 
 
+type Auth {
+    token: ID
+    user: User
 
+}
 
+    type Query {
+        me: User
+    }
 
-`
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+`;
