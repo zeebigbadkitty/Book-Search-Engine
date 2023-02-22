1e48484d286bcba7aaaea363c78aae312e47f635
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-22 12:48:33 -0600

    Importing the mutations that I'm going to create into my index.js.

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { useMutation } from '@apollo/client';
+import {  } from '../../utils/mutations';
+import {  } from '../../utils/queries';
 
 ReactDOM.render(
   <React.StrictMode>
