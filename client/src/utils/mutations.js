bb95a868df112bc1db752d83a2cdbbe8f7c96a30
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-25 17:48:58 -0600

    Created the GET ME query.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,13 @@
 import { gql } from '@apollo/client';
 
+// * `LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
+
+// 	* `ADD_USER` will execute the `addUser` mutation.
+
+// 	* `SAVE_BOOK` will execute the `saveBook` mutation.
+
+// 	* `REMOVE_BOOK` will execute the `removeBook` mutation.
+
 export const CREATE_MATCHUP = gql`
   mutation createMatchup($tech1: String!, $tech2: String!) {
     createMatchup(tech1: $tech1, tech2: $tech2) {
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,22 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_TECH = gql`
-  query tech {
-    tech {
-      _id
-      name
-    }
-  }
-`;
+// This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
 
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
-  }
-`;
+
+export const GET_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            description
+            image
+            link
+            title
+        }
+        }
+}`;
+
