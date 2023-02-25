607f354e5ed08f65136b8d78507d7489e10052df
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-25 17:54:54 -0600

    Add user mutation complete on the front end.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -30,15 +30,29 @@ export const LOGIN_USER = gql`
   }
 `;
 
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        savedBooks {
+          bookId
+          authors
+          description
+          image
+          link
+          title
+
+        }
+      }
     }
   }
-`;
+  `;
 
+export const SAVE_BOOK = gql`
+`;
+export const REMOVE_BOOK = gql`
+`;
